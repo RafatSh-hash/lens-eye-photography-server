43d4c79f8e748e57bcb1ab56a818964e690ffcce
RafatSh-hash <73437594+RafatSh-hash@users.noreply.github.com>
2022-11-08 16:06:41 +0600

    server started with some function

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 1000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5crvfi4.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("lephoto").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.get("/allservices", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(0);
+      const allservices = await cursor.toArray();
+      console.log(allservices);
+      res.send(allservices);
+    });
+
+    app.get("/allservices/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((e) => console.log(e));
+
+app.get("/", (req, res) => {
+  res.send("Server running Successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to ${port}`);
+});
